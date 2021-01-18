59b02a5e926e88417334d291a16048e547acf696
Mike Szczys <szczys@hotmail.com>
2021-01-17 23:43:48 -0600

    Abstracted buttons; wired up actual musicbox

--- musicbox.py
+++ musicbox.py
@@ -27,10 +27,23 @@ import random
 musicdir = "/home/orangepi/musicbox/music/"
 startupsound = "ding.wav.mp3"
 
-#Assign favorites:
+#Assign favorite songs:
 favorite1 = "13 - The Goldfish.mp3"
 favorite2 = "11 - Drivin' in My Car.mp3"
 favorite3 = "02 - Rocketship Run.mp3"
+favorite4 = "08 - Fast And Slow (The Rabbit And The Turtle).mp3"
+favorite5 = "01 - Bumblebee (Buzz Buzz).mp3"
+favorite6 = "04 - We Are The Dinosaurs.mp3"
+
+#Assign Buttons:
+but_play = GPIO.PA+10
+but_stop = GPIO.PA+20
+but_fav1 = GPIO.PA+9
+but_fav2 = GPIO.PA+8
+but_fav3 = GPIO.PA+7
+but_fav4 = GPIO.PC+7
+but_fav5 = GPIO.PC+4
+but_fav6 = GPIO.PD+14
 
 #Button timeout (how long to wait before another button press can happen)
 play_button_timeout = 2
@@ -89,34 +102,55 @@ def reset_playing_flag(status=False):
 #Pinout: https://i.stack.imgur.com/lzt4s.png
 GPIO.setboard(GPIO.PCPCPLUS)        # Orange Pi PC Plus
 GPIO.setmode(GPIO.SOC)        # set up BOARD SOC numbering
-GPIO.setup(GPIO.PA+7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(GPIO.PA+8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(GPIO.PA+9, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(GPIO.PA+10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(GPIO.PA+20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_play, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_stop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_fav1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_fav2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_fav3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_fav4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_fav5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(but_fav6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 killplay()
+print("\n##### Music Box Running and Ready #####\n")
 process = play(startupsound)
 sleep(0.5)
 
 try:
     while True:
-        if not GPIO.input(GPIO.PA+7):
+        if not GPIO.input(but_stop):
+            print("\n##### Stop Button Pressed #####\n")
             reset_playing_flag()
             killplay()
             sleep(stop_button_timeout)
-        elif not GPIO.input(GPIO.PA+8):
+        elif not GPIO.input(but_play):
+            print("\n##### Play Button Pressed #####\n")
             reset_playing_flag(status=True)
             play_next()
-        elif not GPIO.input(GPIO.PA+9):
+        elif not GPIO.input(but_fav1):
+            print("\n##### Favorite1 Button Pressed #####\n")
             reset_playing_flag()
             play(favorite1)
-        elif not GPIO.input(GPIO.PA+10):
+        elif not GPIO.input(but_fav2):
+            print("\n##### Favorite2 Button Pressed #####\n")
             reset_playing_flag()
             play(favorite2)
-        elif not GPIO.input(GPIO.PA+20):
+        elif not GPIO.input(but_fav3):
+            print("\n##### Favorite3 Button Pressed #####\n")
             reset_playing_flag()
             play(favorite3)
+        elif not GPIO.input(but_fav4):
+            print("\n##### Favorite4 Button Pressed #####\n")
+            reset_playing_flag()
+            play(favorite4)
+        elif not GPIO.input(but_fav5):
+            print("\n##### Favorite5 Button Pressed #####\n")
+            reset_playing_flag()
+            play(favorite5)
+        elif not GPIO.input(but_fav6):
+            print("\n##### Favorite6 Button Pressed #####\n")
+            reset_playing_flag()
+            play(favorite6)
 
         #Handle auto-play
         if keep_playing and process.poll() is not None:
